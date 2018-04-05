8e78c786519f4e256bdfbccc11fec3d0b68fdf4c
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2018-04-05 19:00:31 +0530

    draft version ready

--- script.js
+++ script.js
@@ -8,19 +8,28 @@ recognition.lang = 'mr-IN';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-var diagnostic = document.querySelector('.output');
+// var diagnostic = document.querySelector('.output');
 var str = document.querySelector('#string');
 var loc = document.querySelector('#location');
 var targ = document.querySelector('#targ');
+var trans = document.querySelector('#trans');
+var logs = document.querySelector('#log')
 
+var data = {};
+var cursor = 0;
 
-var data = {}
 
 function showData(n){
   var ob = data[n];
   str.textContent = ob['string'];
   loc.textContent = ob['location'];
   targ.textContent = ob['target'];
+  trans.textContent = ""
+}
+
+function setRandom(){
+  cursor = Math.floor(Math.random() * Math.floor(data.length));
+  showData(cursor);
 }
 
 fetch("data/qgis.json")
@@ -30,12 +39,28 @@ fetch("data/qgis.json")
 .then(function(myJson){
   console.log(myJson);
   data = myJson;
+  setRandom();
   })
 
-document.body.onclick = function() {
+function start() {
   recognition.start();
   console.log('Ready to translate.');
+  logs.textContent="listening..."
 }
+
+function copy() {
+  trans.value = targ.textContent;
+}
+
+function save(){
+  data[cursor]["translation"] = trans.value;
+  setRandom();
+}
+
+// document.body.onclick = function() {
+//   recognition.start();
+//   console.log('Ready to translate.');
+// }
 
 recognition.onresult = function(event) {
   // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
@@ -50,8 +75,10 @@ recognition.onresult = function(event) {
   var last = event.results.length - 1;
   var spoken = event.results[last][0].transcript;
 
-  diagnostic.textContent = spoken;
+  // diagnostic.textContent = spoken;
+  trans.value = spoken;
   console.log('Confidence: ' + event.results[0][0].confidence);
+  logs.textContent='';
 }
 
 recognition.onspeechend = function() {
